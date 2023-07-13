a331968f565f3b790b17c499918981ce57a182ff
Thoma Mimani <hmimani@outlook.com>
2023-07-13 15:03:32 +0200

    Rotate the cube functionality

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+import * as THREE from 'three'
+
 // Scene
 const scene = new THREE.Scene()
 
@@ -6,8 +8,8 @@ const geometry = new THREE.BoxGeometry(2, 1, 2);
 const material = new THREE.MeshBasicMaterial({ color: 'red' });
 const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh)
-mesh.position.z = -5
-mesh.position.x = -1
+mesh.position.z = -4
+mesh.position.x = 5
 mesh.position.y = -1
 
 // Border
@@ -38,20 +40,58 @@ renderer.setSize(size.width, size.height)
 document.body.appendChild(renderer.domElement);
 renderer.render(scene, camera)
 
+let rotateEnabled = false; // Flag to indicate if rotation is enabled
+
 window.addEventListener('keydown', (event) => {
     console.log('Key pressed:', event.key);
-    if (event.key === 'ArrowUp') {
-        mesh.position.z -= 0.1
-    }
-    if (event.key === 'ArrowDown') {
-        mesh.position.z += 0.1
-    }
-    if (event.key === 'ArrowLeft') {
-        mesh.position.x -= 0.1
-    }
-    if (event.key === 'ArrowRight') {
-        mesh.position.x += 0.1
-    }
-    renderer.render(scene, camera)
 
+    // Check if 'R' key is pressed
+    if (event.key === 'r' || event.key === 'R') {
+        rotateEnabled = true; // Enable rotation
+    }
+
+    renderer.render(scene, camera);
+});
+
+window.addEventListener('keyup', (event) => {
+    // Check if 'R' key is released
+    if (event.key === 'r' || event.key === 'R') {
+        rotateEnabled = false; // Disable rotation
+    }
+
+    renderer.render(scene, camera);
+});
+
+window.addEventListener('keydown', (event) => {
+    // Check if rotation is enabled and perform rotation
+    if (rotateEnabled) {
+        if (event.key === 'ArrowUp') {
+            mesh.rotation.x -= 0.1;
+        }
+        if (event.key === 'ArrowDown') {
+            mesh.rotation.x += 0.1;
+        }
+        if (event.key === 'ArrowLeft') {
+            mesh.rotation.y -= 0.1;
+        }
+        if (event.key === 'ArrowRight') {
+            mesh.rotation.y += 0.1;
+        }
+
+    } else {
+        if (event.key === 'ArrowUp') {
+            mesh.position.y += 0.1;
+        }
+        if (event.key === 'ArrowDown') {
+            mesh.position.y -= 0.1;
+        }
+        if (event.key === 'ArrowLeft') {
+            mesh.position.x -= 0.1;
+        }
+        if (event.key === 'ArrowRight') {
+            mesh.position.x += 0.1;
+        }
+
+    }
+    renderer.render(scene, camera);
 });
